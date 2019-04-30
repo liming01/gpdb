50128cdcf02f3b52217c70dfbf92759016a811d4
Adam Lee <ali@pivotal.io>
2019-04-30 16:14:31 +0800

    A space after the `if` and no period at the end of error message

--- src/backend/catalog/aclchk.c
+++ src/backend/catalog/aclchk.c
@@ -5050,14 +5050,10 @@ pg_database_aclcheck(Oid db_oid, Oid roleid, AclMode mode)
 AclResult
 pg_proc_aclcheck(Oid proc_oid, Oid roleid, AclMode mode)
 {
-	/* Only builtin function can be called in retrieve mode */
-	if (Gp_role == GP_ROLE_RETRIEVE)
-	{
-		if(proc_oid >= FirstNormalObjectId)
-		{
-			elog(ERROR, "Only builtin function can be called in retrieve mode.");
-		}
-	}
+	/* Only builtin function can be called for retrieve role */
+	if ((Gp_role == GP_ROLE_RETRIEVE) && (proc_oid >= FirstNormalObjectId))
+		elog(ERROR, "Only builtin functions can be called for retrieve role");
+
 	if (pg_proc_aclmask(proc_oid, roleid, mode, ACLMASK_ANY) != 0)
 		return ACLCHECK_OK;
 	else
